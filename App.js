a0c76bfa12ac867b6b2f9bc5cb46924244a47d55
Lore Marissens <lore.marissens@gmail.com>
2023-01-15 22:23:38 +0100

    examen dev

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+
+import BusTrips from './screens/BusTrips';
+import InfoDetail from './screens/InfoDetail';
+import Winkelwagen from './screens/Winkelwagen';
+import Uno from './screens/Uno';
+import Rummikub from './screens/Rummikub';
+import Qwirkle from './screens/Qwirkle';
+
+const Stack = createNativeStackNavigator();
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Games for you" component={(BusTrips)} />
+        <Stack.Screen name="Info" component={(InfoDetail)} />
+        <Stack.Screen name="Winkelwagen" component={(Winkelwagen)} />
+        <Stack.Screen name="Uno" component={(Uno)} />
+        <Stack.Screen name="Rummikub" component={(Rummikub)} />
+        <Stack.Screen name="Qwirkle" component={(Qwirkle)} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
